8aa162ccbc7c6a7a3e202726689b0937b98aea78
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-07-13 07:37:39 -0700

    ollp

--- src/app/components/testimonial/Testimonial.component.tsx
+++ src/app/components/testimonial/Testimonial.component.tsx
@@ -1,15 +1,103 @@
 import React from "react";
 import "./Testimonial.component.scss";
-import img5403 from "../../../assets/images/Group 5403.png";
+import img5403 from "../../../assets/images/smiling-woman-with-afro-posing-pink-sweater 1.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faStar } from "@fortawesome/free-solid-svg-icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper";
 
 const Testimonial = () => {
   return (
     <>
       <div className="container">
         <div className="testimonial">
-          <img src={img5403} alt="" />
+          <Swiper navigation={true} modules={[Navigation]} className="carousel">
+            <SwiperSlide className="slide">
+              <img src={img5403} alt="" />
+              <div className="box">
+                <div className="text">
+                  "Thank you so much for your help. It's exactly what I've been
+                  looking for. You won't regret it. It really saves me time and
+                  effort. Skilline is exactly what our business has been
+                  lacking."
+                </div>
+                <div className="row">
+                  <div className="name">Gloria Rose</div>
+                  <div className="rate">
+                    <div className="stars">
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                    </div>
+                    <div className="people">12 reviews at Yelp</div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <img src={img5403} alt="" />
+              <div className="box">
+                <div className="text">
+                  "Thank you so much for your help. It's exactly what I've been
+                  looking for. You won't regret it. It really saves me time and
+                  effort. Skilline is exactly what our business has been
+                  lacking."
+                </div>
+                <div className="row">
+                  <div className="name">Gloria Rose</div>
+                  <div className="rate">
+                    <div className="stars">
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon
+                        style={{ color: "grey" }}
+                        icon={faStar}
+                      />
+                      <FontAwesomeIcon
+                        style={{ color: "grey" }}
+                        icon={faStar}
+                      />
+                    </div>
+                    <div className="people">5 reviews at Yelp</div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <img src={img5403} alt="" />
+              <div className="box">
+                <div className="text">
+                  "Thank you so much for your help. It's exactly what I've been
+                  looking for. You won't regret it. It really saves me time and
+                  effort. Skilline is exactly what our business has been
+                  lacking."
+                </div>
+                <div className="row">
+                  <div className="name">Gloria Rose</div>
+                  <div className="rate">
+                    <div className="stars">
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon icon={faStar} />
+                      <FontAwesomeIcon
+                        style={{ color: "grey" }}
+                        icon={faStar}
+                      />
+                    </div>
+                    <div className="people">32 reviews at Yelp</div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
           <div className="box">
             <div className="label">TESTIMONIAL</div>
             <h2>What They Say?</h2>
